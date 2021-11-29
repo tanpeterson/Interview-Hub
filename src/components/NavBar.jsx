afab3ecdd4b0a69fa4ab57eef4e205eefeed2929
tanpeterson <tanpeterson@gmail.com>
2021-11-29 15:56:16 -0800

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { Container, NavBar, Nav, NavDropdown } from 'react-bootstrap'
 
 export default function NavBar() {
   return (
-    <nav>
-    <ul>
-      <li> <NavLink exact activeClassName="current" to='/Home'> HOME </NavLink></li>
-      <li> <NavLink exact activeClassName="current" to='/Profile'> Profile </NavLink></li>
-    </ul>
-  </nav>
+    <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Profile</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   )
 }
